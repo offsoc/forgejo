8a925695b23bfff0c6ef5dc36b2f0f40244fb454
dragon <ExplodingFKL@gmail.com>
2024-09-14 09:40:21 +0800

    fix force update tags

--- services/repository/push.go
+++ services/repository/push.go
@@ -309,8 +309,9 @@ func pushUpdateAddTags(ctx context.Context, repo *repo_model.Repository, gitRepo
 	}
 
 	releases, err := db.Find[repo_model.Release](ctx, repo_model.FindReleasesOptions{
-		RepoID:   repo.ID,
-		TagNames: tags,
+		RepoID:      repo.ID,
+		TagNames:    tags,
+		IncludeTags: true,
 	})
 	if err != nil {
 		return fmt.Errorf("db.Find[repo_model.Release]: %w", err)
