c691e95eb8d475ba312011377d5b6654737a118d
JakobDev <jakobdev@gmx.de>
2024-10-08 18:48:19 +0200

    Fix

--- modules/git/notes_test.go
+++ modules/git/notes_test.go
@@ -16,14 +16,23 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+const (
+	testReposDir = "tests/repos/"
+)
+
+// openRepositoryWithDefaultContext opens the repository at the given path with DefaultContext.
+func openRepositoryWithDefaultContext(repoPath string) (*git.Repository, error) {
+	return git.OpenRepository(git.DefaultContext, repoPath)
+}
+
 func TestGetNotes(t *testing.T) {
 	bareRepo1Path := filepath.Join(testReposDir, "repo1_bare")
 	bareRepo1, err := openRepositoryWithDefaultContext(bareRepo1Path)
 	require.NoError(t, err)
 	defer bareRepo1.Close()
 
-	note := Note{}
-	err = GetNote(context.Background(), bareRepo1, "95bb4d39648ee7e325106df01a621c530863a653", &note)
+	note := git.Note{}
+	err = git.GetNote(context.Background(), bareRepo1, "95bb4d39648ee7e325106df01a621c530863a653", &note)
 	require.NoError(t, err)
 	assert.Equal(t, []byte("Note contents\n"), note.Message)
 	assert.Equal(t, "Vladimir Panteleev", note.Commit.Author.Name)
@@ -35,11 +44,11 @@ func TestGetNestedNotes(t *testing.T) {
 	require.NoError(t, err)
 	defer repo.Close()
 
-	note := Note{}
-	err = GetNote(context.Background(), repo, "3e668dbfac39cbc80a9ff9c61eb565d944453ba4", &note)
+	note := git.Note{}
+	err = git.GetNote(context.Background(), repo, "3e668dbfac39cbc80a9ff9c61eb565d944453ba4", &note)
 	require.NoError(t, err)
 	assert.Equal(t, []byte("Note 2"), note.Message)
-	err = GetNote(context.Background(), repo, "ba0a96fa63532d6c5087ecef070b0250ed72fa47", &note)
+	err = git.GetNote(context.Background(), repo, "ba0a96fa63532d6c5087ecef070b0250ed72fa47", &note)
 	require.NoError(t, err)
 	assert.Equal(t, []byte("Note 1"), note.Message)
 }
--- modules/git/repo_base_test.go
+++ modules/git/repo_base_test.go
@@ -161,20 +161,3 @@ func TestCatFileBatchCheck(t *testing.T) {
 		require.Error(t, err)
 	})
 }
-// Copyright 2024 The Forgejo Authors. All rights reserved.
-// SPDX-License-Identifier: MIT
-
-package git_test
-
-import (
-	"code.gitea.io/gitea/modules/git"
-)
-
-const (
-	testReposDir = "tests/repos/"
-)
-
-// openRepositoryWithDefaultContext opens the repository at the given path with DefaultContext.
-func openRepositoryWithDefaultContext(repoPath string) (*git.Repository, error) {
-	return git.OpenRepository(git.DefaultContext, repoPath)
-}
