46b41bf76fa79fc5d19906bf0f878463bbdb7925
emilylange <git@emilylange.de>
2024-12-25 16:12:09 +0100

    feat(performance): enable bloom filters when writing commit-graph
    Git 2.27.0 (May, 2020) added initial support for bloom filters allowing
    for faster lookups and operations involving history of paths like
    git blame, git log, and since then in later releases rev-list.
    
    On a recent Forgejo checkout, `git rev-list HEAD --count -- templates`
    takes ~100ms with the bloom filters enabled (--changed-paths) and ~140ms
    with them disabled (--no-changed-paths).
    
    On a recent NixOS/nixpkgs checkout, the same command but with `-- nixos`
    as path sees improvements of roughly half a second (~2.08s -> 1.47s).
    
    There is additional compute associated to compute those bloom filters.
    
    From my testing on something as big as nixpkgs (700k commits) it adds
    something like 70 seconds to the initial commit-graph write. After that,
    the write is incremental and reuses the previously computed filters.
    
    Ref <https://git-scm.com/docs/git-commit-graph>

--- modules/git/repo_commitgraph.go
+++ modules/git/repo_commitgraph.go
@@ -9,10 +9,15 @@ import (
 )
 
 // WriteCommitGraph write commit graph to speed up repo access
-// this requires git v2.18 to be installed
+// this requires git v2.18 to be installed and optionally
+// git v2.27 to enable bloom filters
 func WriteCommitGraph(ctx context.Context, repoPath string) error {
 	if CheckGitVersionAtLeast("2.18") == nil {
-		if _, _, err := NewCommand(ctx, "commit-graph", "write").RunStdString(&RunOpts{Dir: repoPath}); err != nil {
+		cmd := NewCommand(ctx, "commit-graph", "write")
+		if CheckGitVersionAtLeast("2.27") == nil {
+			cmd.AddArguments("--changed-paths")
+		}
+		if _, _, err := cmd.RunStdString(&RunOpts{Dir: repoPath}); err != nil {
 			return fmt.Errorf("unable to write commit-graph for '%s' : %w", repoPath, err)
 		}
 	}
