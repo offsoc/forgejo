a4a28f779f2d845fe2080881a4c8fdb4bccddad0
Thilina Jayanath <thilina91@gmail.com>
2025-01-02 13:36:41 +0100

    add integration test for migration with releases

--- tests/integration/migrate_test.go
+++ tests/integration/migrate_test.go
@@ -5,6 +5,7 @@
 package integration
 
 import (
+	"context"
 	"fmt"
 	"net/http"
 	"net/url"
@@ -16,6 +17,7 @@ import (
 	"code.gitea.io/gitea/models/db"
 	issues_model "code.gitea.io/gitea/models/issues"
 	repo_model "code.gitea.io/gitea/models/repo"
+	"code.gitea.io/gitea/models/unit"
 	"code.gitea.io/gitea/models/unittest"
 	user_model "code.gitea.io/gitea/models/user"
 	"code.gitea.io/gitea/modules/setting"
@@ -165,7 +167,68 @@ func TestMigrateWithWiki(t *testing.T) {
 				// Step 6: check the repo was created and load the repo
 				migratedRepo := unittest.AssertExistsAndLoadBean(t, &repo_model.Repository{Name: migratedRepoName})
 				// Step 7: check if the wiki is enabled
-				if !migratedRepo.HasWiki() {
+				if !migratedRepo.UnitEnabled(context.TODO(), unit.TypeWiki) {
+					t.Errorf("Expected wiki to be enabled in the migrated repo. Wiki is disabled")
+				}
+			})
+		}
+	})
+}
+
+func TestMigrateWithReleases(t *testing.T) {
+	onGiteaRun(t, func(t *testing.T, u *url.URL) {
+		defer test.MockVariableValue(&setting.Migrations.AllowLocalNetworks, true)()
+		defer test.MockVariableValue(&setting.AppVer, "1.16.0")()
+		require.NoError(t, migrations.Init())
+
+		ownerName := "user2"
+		repoName := "repo1"
+		repoOwner := unittest.AssertExistsAndLoadBean(t, &user_model.User{Name: ownerName})
+		session := loginUser(t, ownerName)
+		token := getTokenForLoggedInUser(t, session, auth_model.AccessTokenScopeWriteRepository, auth_model.AccessTokenScopeReadMisc)
+
+		for _, s := range []struct {
+			svc structs.GitServiceType
+		}{
+			{svc: structs.GiteaService},
+			{svc: structs.ForgejoService},
+		} {
+			t.Run(s.svc.Name(), func(t *testing.T) {
+				defer tests.PrintCurrentTest(t)()
+				// Step 0: verify the repo is available
+				req := NewRequestf(t, "GET", "/%s/%s", ownerName, repoName)
+				_ = session.MakeRequest(t, req, http.StatusOK)
+				// Step 1: get the Gitea migration form
+				req = NewRequestf(t, "GET", "/repo/migrate/?service_type=%d", s.svc)
+				resp := session.MakeRequest(t, req, http.StatusOK)
+				// Step 2: load the form
+				htmlDoc := NewHTMLParser(t, resp.Body)
+				// Check form title
+				title := htmlDoc.doc.Find("title").Text()
+				assert.Contains(t, title, translation.NewLocale("en-US").TrString("new_migrate.title"))
+				// Get the link of migration button
+				link, exists := htmlDoc.doc.Find(`form.ui.form[action^="/repo/migrate"]`).Attr("action")
+				assert.True(t, exists, "The template has changed")
+				// Step 4: submit the migration to only migrate issues
+				migratedRepoName := "otherrepo-" + s.svc.Name()
+				req = NewRequestWithValues(t, "POST", link, map[string]string{
+					"_csrf":       htmlDoc.GetCSRF(),
+					"service":     fmt.Sprintf("%d", s.svc),
+					"clone_addr":  fmt.Sprintf("%s%s/%s", u, ownerName, repoName),
+					"auth_token":  token,
+					"issues":      "on",
+					"releases":    "on",
+					"repo_name":   migratedRepoName,
+					"description": "",
+					"uid":         fmt.Sprintf("%d", repoOwner.ID),
+				})
+				resp = session.MakeRequest(t, req, http.StatusSeeOther)
+				// Step 5: a redirection displays the migrated repository
+				assert.EqualValues(t, fmt.Sprintf("/%s/%s", ownerName, migratedRepoName), test.RedirectURL(resp))
+				// Step 6: check the repo was created and load the repo
+				migratedRepo := unittest.AssertExistsAndLoadBean(t, &repo_model.Repository{Name: migratedRepoName})
+				// Step 7: check if the wiki is enabled
+				if !migratedRepo.UnitEnabled(context.TODO(), unit.TypeReleases) {
 					t.Errorf("Expected wiki to be enabled in the migrated repo. Wiki is disabled")
 				}
 			})
