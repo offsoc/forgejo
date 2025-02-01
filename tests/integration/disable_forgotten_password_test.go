363e2110ebf7472f079ad33e783f9f86ecea99cb
davrot <davrot@noreply.codeberg.org>
2025-02-01 23:16:42 +0000

    Dateien nach „tests/integration“ hochladen

---
+++ tests/integration/disable_forgotten_password_test.go
@@ -0,0 +1,71 @@
+package integration
+
+import (
+	"net/http"
+	"testing"
+
+	"code.gitea.io/gitea/modules/setting"
+	"code.gitea.io/gitea/modules/test"
+	"code.gitea.io/gitea/tests"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDisableForgottenPasswordTrue(t *testing.T) {
+	defer tests.PrepareTestEnv(t)()
+	defer test.MockVariableValue(&setting.Service.SignInForgottenPasswordEnabled, true)()
+
+	req := NewRequest(t, "GET", "/user/login/")
+	resp := MakeRequest(t, req, http.StatusOK)
+	doc := NewHTMLParser(t, resp.Body).Find("a")
+	var counterInstances int = 0
+	for i := range doc.Nodes {
+		oneElement := doc.Eq(i)
+		attValue, attExists := oneElement.Attr("href")
+		if attExists {
+			if attValue == "/user/forgot_password" {
+				counterInstances += 1
+			}
+		}
+	}
+	assert.EqualValues(t, 1, counterInstances)
+}
+
+func TestDisableForgottenPasswordDefault(t *testing.T) {
+	defer tests.PrepareTestEnv(t)()
+
+	req := NewRequest(t, "GET", "/user/login/")
+	resp := MakeRequest(t, req, http.StatusOK)
+	doc := NewHTMLParser(t, resp.Body).Find("a")
+	var counterInstances int = 0
+	for i := range doc.Nodes {
+		oneElement := doc.Eq(i)
+		attValue, attExists := oneElement.Attr("href")
+		if attExists {
+			if attValue == "/user/forgot_password" {
+				counterInstances += 1
+			}
+		}
+	}
+	assert.EqualValues(t, 1, counterInstances)
+}
+
+func TestDisableForgottenPasswordFalse(t *testing.T) {
+	defer tests.PrepareTestEnv(t)()
+	defer test.MockVariableValue(&setting.Service.SignInForgottenPasswordEnabled, false)()
+
+	req := NewRequest(t, "GET", "/user/login/")
+	resp := MakeRequest(t, req, http.StatusOK)
+	doc := NewHTMLParser(t, resp.Body).Find("a")
+	var counterInstances int = 0
+	for i := range doc.Nodes {
+		oneElement := doc.Eq(i)
+		attValue, attExists := oneElement.Attr("href")
+		if attExists {
+			if attValue == "/user/forgot_password" {
+				counterInstances += 1
+			}
+		}
+	}
+	assert.EqualValues(t, 0, counterInstances)
+}
