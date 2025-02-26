93f67707a4d479316a8e7470fda0c69ca626fdb9
Michael Jerger <michael.jerger@meissa-gmbh.de>
2025-02-26 15:00:20 +0100

    add integration test for follow operation

---
+++ tests/integration/api_user_follow_federated_test.go
@@ -0,0 +1,45 @@
+// Copyright 2022 The Gitea Authors. All rights reserved.
+// SPDX-License-Identifier: MIT
+
+package integration
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+
+	auth_model "code.gitea.io/gitea/models/auth"
+	"code.gitea.io/gitea/modules/setting"
+	"code.gitea.io/gitea/modules/test"
+	"code.gitea.io/gitea/routers"
+	"code.gitea.io/gitea/tests"
+	ttools "code.gitea.io/gitea/tests/tools"
+)
+
+func TestAPIFollowFederated(t *testing.T) {
+	defer test.MockVariableValue(&setting.Federation.Enabled, true)()
+	defer test.MockVariableValue(&testWebRoutes, routers.NormalRoutes())()
+	defer tests.PrepareTestEnv(t)()
+
+	federatedSrv := ttools.DistantFederationServer(t)
+	defer federatedSrv.Close()
+
+	//user1 := "user4"
+	user2 := "user10"
+
+	//session1 := loginUser(t, user1)
+	//token1 := getTokenForLoggedInUser(t, session1, auth_model.AccessTokenScopeReadUser)
+
+	session2 := loginUser(t, user2)
+	token2 := getTokenForLoggedInUser(t, session2, auth_model.AccessTokenScopeWriteUser)
+
+	activity := fmt.Sprintf(`{"target":"%s/api/v1/activitypub/user-id/15"}`, federatedSrv.URL)
+
+	t.Run("Follow", func(t *testing.T) {
+		defer tests.PrintCurrentTest(t)()
+
+		req := NewRequestWithJSON(t, "POST", "/api/v1/user/activitypub/follow", activity).
+			AddTokenAuth(token2)
+		MakeRequest(t, req, http.StatusNoContent)
+	})
+}
