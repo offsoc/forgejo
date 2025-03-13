9d02cc65f6a305c2ec585b0c716f49e996bf3d33
Michael Jerger <michael.jerger@meissa-gmbh.de>
2025-03-13 13:28:00 +0100

    add migration for normalized_federated_uri

--- models/migrations/migrations.go
+++ models/migrations/migrations.go
@@ -366,6 +366,8 @@ func prepareMigrationTasks() []*migration {
 		// Migration to Forgejo v10
 		newMigration(303, "Gitea last drop", v1_23.GiteaLastDrop),
 		newMigration(304, "Migrate `secret` column to store keying material", forgejo_migrations.MigrateTwoFactorToKeying),
+
+		newMigration(305, "Migrate `User.NormalizedFederatedURI` column to extract port & schema into FederatedHost", v1_23.MigrateNormalizedFederatedURI),
 	}
 	return preparedMigrations
 }
