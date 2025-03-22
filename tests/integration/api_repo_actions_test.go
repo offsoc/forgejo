bf9e07e570e9797cb814d38566b8df49c6ed8ec6
Klaus Fyhn <klfj@mir-robots.com>
2025-03-22 13:02:53 +0100

    fix-test: test needed to be wrapped into with giteaRun

--- tests/integration/api_repo_actions_test.go
+++ tests/integration/api_repo_actions_test.go
@@ -6,6 +6,7 @@ package integration
 import (
 	"fmt"
 	"net/http"
+	"net/url"
 	"strings"
 	"testing"
 
@@ -47,55 +48,56 @@ func TestAPISearchActionJobs_RepoRunner(t *testing.T) {
 }
 
 func TestAPIWorkflowDispatchReturnInfo(t *testing.T) {
-	defer tests.PrepareTestEnv(t)()
+	onGiteaRun(t, func(t *testing.T, u *url.URL) {
+		workflowName := "dispatch.yml"
+		user2 := unittest.AssertExistsAndLoadBean(t, &user_model.User{ID: 2})
+		token := getUserToken(t, user2.LowerName, auth_model.AccessTokenScopeWriteRepository)
 
-	workflowName := "dispatch.yml"
-	user2 := unittest.AssertExistsAndLoadBean(t, &user_model.User{ID: 2})
-	token := getUserToken(t, user2.LowerName, auth_model.AccessTokenScopeWriteRepository)
-	// create the repo
-	repo, sha, f := tests.CreateDeclarativeRepo(t, user2, "api-repo-workflow-dispatch",
-		[]unit_model.Type{unit_model.TypeActions}, nil,
-		[]*files_service.ChangeRepoFile{
-			{
-				Operation: "create",
-				TreePath:  fmt.Sprintf(".forgejo/workflows/%s", workflowName),
-				ContentReader: strings.NewReader(`name: WD
+		// create the repo
+		repo, _, f := tests.CreateDeclarativeRepo(t, user2, "api-repo-workflow-dispatch",
+			[]unit_model.Type{unit_model.TypeActions}, nil,
+			[]*files_service.ChangeRepoFile{
+				{
+					Operation: "create",
+					TreePath:  fmt.Sprintf(".forgejo/workflows/%s", workflowName),
+					ContentReader: strings.NewReader(`name: WD
 on: [workflow-dispatch]
 jobs:
-	t1:
-		runs-on: docker
-		steps:
-			- run: echo "test 1"
-	t2:
-		runs-on: docker
-		steps:
-			- run: echo "test 2"
+  t1:
+    runs-on: docker
+    steps:
+      - run: echo "test 1"
+  t2:
+    runs-on: docker
+    steps:
+      - run: echo "test 2"
 `,
-				),
+					),
+				},
 			},
-		},
-	)
-	defer f()
+		)
+		defer f()
 
-	req := NewRequestWithJSON(
-		t,
-		http.MethodPost,
-		fmt.Sprintf(
-			"/api/v1/repos/%s/%s/actions/workflows/%s/dispatches",
-			repo.OwnerName, repo.Name, workflowName,
-		),
-		&api.DispatchWorkflowOption{
-			Ref:           sha,
-			ReturnRunInfo: true,
-		},
-	)
-	req.AddTokenAuth(token)
+		req := NewRequestWithJSON(
+			t,
+			http.MethodPost,
+			fmt.Sprintf(
+				"/api/v1/repos/%s/%s/actions/workflows/%s/dispatches",
+				repo.OwnerName, repo.Name, workflowName,
+			),
+			&api.DispatchWorkflowOption{
+				Ref:           repo.DefaultBranch,
+				ReturnRunInfo: true,
+			},
+		)
+		req.AddTokenAuth(token)
 
-	res := MakeRequest(t, req, http.StatusCreated)
-	run := new(api.DispatchWorkflowRun)
-	DecodeJSON(t, res, run)
+		res := MakeRequest(t, req, http.StatusCreated)
+		run := new(api.DispatchWorkflowRun)
+		DecodeJSON(t, res, run)
 
-	assert.NotZero(t, run.ID)
-	assert.NotZero(t, run.RunNumber)
-	assert.Len(t, run.Jobs, 2)
+		assert.NotZero(t, run.ID)
+		assert.NotZero(t, run.RunNumber)
+		assert.Len(t, run.Jobs, 2)
+	})
 }
