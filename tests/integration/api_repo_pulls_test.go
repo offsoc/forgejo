9e433386d47cdeb8b686f61b6582159bede5aeef
ThomasBoom89 <thomasboom89@noreply.codeberg.org>
2025-03-27 22:02:05 +0100

    Codestyle: append gofumpt and revive fixes

--- tests/integration/api_repo_pulls_test.go
+++ tests/integration/api_repo_pulls_test.go
@@ -4,14 +4,16 @@
 package integration
 
 import (
+	"fmt"
+	"net/http"
+	"testing"
+
 	repo_model "code.gitea.io/gitea/models/repo"
 	"code.gitea.io/gitea/models/unittest"
 	api "code.gitea.io/gitea/modules/structs"
 	"code.gitea.io/gitea/tests"
-	"fmt"
+
 	"github.com/stretchr/testify/assert"
-	"net/http"
-	"testing"
 )
 
 func TestAPIRepoPulls(t *testing.T) {
@@ -21,8 +23,8 @@ func TestAPIRepoPulls(t *testing.T) {
 	repo := unittest.AssertExistsAndLoadBean(t, &repo_model.Repository{ID: 1})
 
 	// issue id without assigned review member or review team
-	issueId := 5
-	req := NewRequest(t, "GET", fmt.Sprintf("/api/v1/repos/%s/%s/pulls/%d", repo.OwnerName, repo.Name, issueId))
+	issueID := 5
+	req := NewRequest(t, "GET", fmt.Sprintf("/api/v1/repos/%s/%s/pulls/%d", repo.OwnerName, repo.Name, issueID))
 	res := MakeRequest(t, req, http.StatusOK)
 	var pr *api.PullRequest
 	DecodeJSON(t, res, &pr)
