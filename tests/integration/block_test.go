b0dd490ae126ea614131cf2e8400ffcd6e34b7d0
Ellen Emilia Anna Zscheile <fogti+devel@ytrizja.de>
2025-03-30 21:58:34 +0000

    feat(repo,locale): merge PR/issues cases for some repo/issue strings (#7092)
    - Merge `archive.nocomment` and `comment.blocked_by_user`'s variants for issue and pull request to a unified string and no longer have a issue and pull request variant.
    - Fixes #6443
    
    Signed-off-by: Ellen Emilia Anna Zscheile <fogti+devel@ytrizja.de>
    Reviewed-on: https://codeberg.org/forgejo/forgejo/pulls/7092
    Reviewed-by: Panagiotis "Ivory" Vasilopoulos <git@n0toose.net>
    Reviewed-by: Gusted <gusted@noreply.codeberg.org>
    Co-authored-by: Ellen Emilia Anna Zscheile <fogti+devel@ytrizja.de>
    Co-committed-by: Ellen Emilia Anna Zscheile <fogti+devel@ytrizja.de>

--- tests/integration/block_test.go
+++ tests/integration/block_test.go
@@ -236,7 +236,7 @@ func TestBlockActions(t *testing.T) {
 	// Ensures that comment creation on doer's owned repositories and doer's
 	// posted issues are blocked.
 	t.Run("Comment creation", func(t *testing.T) {
-		expectedMessage := locale.Tr("repo.issues.comment.blocked_by_user")
+		expectedMessage := locale.Tr("repo.comment.blocked_by_user")
 
 		t.Run("Blocked by repository owner", func(t *testing.T) {
 			defer tests.PrintCurrentTest(t)()
@@ -258,7 +258,7 @@ func TestBlockActions(t *testing.T) {
 			resp = session.MakeRequest(t, req, http.StatusOK)
 			htmlDoc := NewHTMLParser(t, resp.Body)
 			msg := htmlDoc.doc.Find("div .warning").Text()
-			assert.Contains(t, msg, "You cannot comment on this issue because you are blocked")
+			assert.Contains(t, msg, expectedMessage)
 		})
 
 		t.Run("Blocked by issue poster", func(t *testing.T) {
@@ -285,7 +285,7 @@ func TestBlockActions(t *testing.T) {
 			resp = session.MakeRequest(t, req, http.StatusOK)
 			htmlDoc := NewHTMLParser(t, resp.Body)
 			msg := htmlDoc.doc.Find("div .warning").Text()
-			assert.Contains(t, msg, "You cannot comment on this issue because you are blocked")
+			assert.Contains(t, msg, expectedMessage)
 		})
 	})
 
