1ed41b2311b0cb11c739509ba3170e5d5270e235
Michael Jerger <michael.jerger@meissa-gmbh.de>
2025-04-03 16:24:07 +0200

    add copyright

--- modules/validation/validatable.go
+++ modules/validation/validatable.go
@@ -1,5 +1,4 @@
-// Copyright 2024 The Forgejo Authors. All rights reserved.
-// Copyright 2023 The Forgejo Authors. All rights reserved.
+// Copyright 2023, 2024, 2025 The Forgejo Authors. All rights reserved.
 // SPDX-License-Identifier: MIT
 
 package validation
