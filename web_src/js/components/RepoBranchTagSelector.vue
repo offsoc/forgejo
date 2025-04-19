offsoc/forgejo

commit 3f673ce4d4665eb2251206bb5ee6c05c2aac0625

fix(ui): show commit icon in branch dropdown button when viewing a commit (#7571)
Closes https://codeberg.org/forgejo/forgejo/issues/7570

Fixed never running repo-home e2e test for language switcher

Reviewed-on: https://codeberg.org/forgejo/forgejo/pulls/7571
Reviewed-by: 0ko <0ko@noreply.codeberg.org>
Co-authored-by: Beowulf <beowulf@beocode.eu>
Co-committed-by: Beowulf <beowulf@beocode.eu>

web_src/js/components/RepoBranchTagSelector.vue
@@ -54,12 +54,12 @@ const sfc = {
     if (this.viewType === 'tree') {
       this.isViewTree = true;
       this.refNameText = this.commitIdShort;
-    } else if (this.viewType === 'tag') {
+    } else if (this.viewType === 'branch') {
+      this.isViewBranch = true;
+      this.refNameText = this.viewType;
+    } else {
       this.isViewTag = true;
       this.refNameText = this.tagName;
-    } else {
-      this.isViewBranch = true;
-      this.refNameText = this.branchName;
     }
 
     document.body.addEventListener('click', (event) => {
@@ -252,7 +252,8 @@ export default sfc; // activate IDE's Vue plugin
         <template v-if="release">{{ textReleaseCompare }}</template>
         <template v-else>
           <svg-icon v-if="isViewTag" name="octicon-tag"/>
-          <svg-icon v-else name="octicon-git-branch"/>
+          <svg-icon v-else-if="isViewBranch" name="octicon-git-branch"/>
+          <svg-icon v-else name="octicon-git-commit"/>
           <strong ref="dropdownRefName" class="tw-ml-2 tw-inline-block gt-ellipsis">{{ refNameText }}</strong>
         </template>
       </span>

tests/e2e/repo-home.test.e2e.ts
@@ -1,0 +1,33 @@
+// @watch start
+// web_src/js/components/RepoBranchTagSelector.vue
+// web_src/js/features/common-global.js
+// web_src/css/repo.css
+// @watch end
+
+import {expect} from '@playwright/test';
+import {save_visual, test} from './utils_e2e.ts';
+
+test('Language stats bar', async ({page}) => {
+  const response = await page.goto('/user2/language-stats-test');
+  expect(response?.status()).toBe(200);
+
+  await expect(page.locator('#language-stats-legend')).toBeHidden();
+
+  await page.click('#language-stats-bar');
+  await expect(page.locator('#language-stats-legend')).toBeVisible();
+  await save_visual(page);
+
+  await page.click('#language-stats-bar');
+  await expect(page.locator('#language-stats-legend')).toBeHidden();
+  await save_visual(page);
+});
+
+test('Branch selector commit icon', async ({page}) => {
+  const response = await page.goto('/user2/repo1');
+  expect(response?.status()).toBe(200);
+
+  await expect(page.locator('.branch-dropdown-button svg.octicon-git-branch')).toBeVisible();
+
+  await page.goto('/user2/repo1/src/commit/65f1bf27bc');
+  await expect(page.locator('.branch-dropdown-button svg.octicon-git-commit')).toBeVisible();
+});

templates/repo/branch_dropdown.tmpl
@@ -77,8 +77,10 @@
 				{{else}}
 					{{if eq $type "tag"}}
 						{{svg "octicon-tag"}}
-					{{else}}
+					{{else if eq $type "branch"}}
 						{{svg "octicon-git-branch"}}
+					{{else}}
+						{{svg "octicon-git-commit"}}
 					{{end}}
 					<strong ref="dropdownRefName" class="tw-ml-2 tw-inline-block gt-ellipsis">{{if and .root.IsViewTag (not .noTag)}}{{.root.TagName}}{{else if .root.IsViewBranch}}{{.root.BranchName}}{{else}}{{ShortSha .root.CommitID}}{{end}}</strong>
 				{{end}}

tests/e2e/declare_repos_test.go
@@ -13,6 +13,7 @@ import (
 	"forgejo.org/models/unittest"
 	user_model "forgejo.org/models/user"
 	"forgejo.org/modules/git"
+	"forgejo.org/modules/indexer/stats"
 	files_service "forgejo.org/services/repository/files"
 	"forgejo.org/tests"
 
@@ -35,6 +36,10 @@ func DeclareGitRepos(t *testing.T) func() {
 		newRepo(t, 2, "diff-test", []FileChanges{{
 			Filename: "testfile",
 			Versions: []string{"hello", "hallo", "hola", "native", "ubuntu-latest", "- runs-on: ubuntu-latest", "- runs-on: debian-latest"},
+		}}),
+		newRepo(t, 2, "language-stats-test", []FileChanges{{
+			Filename: "main.rs",
+			Versions: []string{"fn main() {", "println!(\"Hello World!\");", "}"},
 		}}),
 		newRepo(t, 2, "mentions-highlighted", []FileChanges{
 			{
@@ -104,6 +109,9 @@ func newRepo(t *testing.T, userID int64, repoName string, fileChanges []FileChan
 			assert.NotEmpty(t, resp)
 		}
 	}
+
+	err := stats.UpdateRepoIndexer(somerepo)
+	require.NoError(t, err)
 
 	return cleanupFunc
 }

tests/e2e/repo-home.e2e.ts
@@ -1,19 +1,0 @@
-// @watch start
-// web_src/js/features/common-global.js
-// web_src/css/repo.css
-// @watch end
-
-import {expect} from '@playwright/test';
-import {save_visual, test} from './utils_e2e.ts';
-
-test('Language stats bar', async ({page}) => {
-  const response = await page.goto('/user2/repo1');
-  expect(response?.status()).toBe(200);
-
-  await expect(page.locator('#language-stats-legend')).toBeVisible();
-  await save_visual(page);
-
-  await page.click('#language-stats-bar');
-  await expect(page.locator('#language-stats-legend')).toBeHidden();
-  await save_visual(page);
-});

commit 876ffdb7141ef971ae24b98016cf1429fc952e40

fix(ui): use correct branch name in branch tag selector
Regression from: 3f673ce4d4665eb2251206bb5ee6c05c2aac0625

web_src/js/components/RepoBranchTagSelector.vue
@@ -56,7 +56,7 @@ const sfc = {
       this.refNameText = this.commitIdShort;
     } else if (this.viewType === 'branch') {
       this.isViewBranch = true;
-      this.refNameText = this.viewType;
+      this.refNameText = this.branchName;
     } else {
       this.isViewTag = true;
       this.refNameText = this.tagName;

tests/e2e/repo-home.test.e2e.ts
@@ -27,7 +27,9 @@ test('Branch selector commit icon', async ({page}) => {
   expect(response?.status()).toBe(200);
 
   await expect(page.locator('.branch-dropdown-button svg.octicon-git-branch')).toBeVisible();
+  await expect(page.locator('.branch-dropdown-button')).toHaveText('master');
 
-  await page.goto('/user2/repo1/src/commit/65f1bf27bc');
+  await page.goto('/user2/repo1/src/commit/65f1bf27bc3bf70f64657658635e66094edbcb4d');
   await expect(page.locator('.branch-dropdown-button svg.octicon-git-commit')).toBeVisible();
+  await expect(page.locator('.branch-dropdown-button')).toHaveText('65f1bf27bc');
 });
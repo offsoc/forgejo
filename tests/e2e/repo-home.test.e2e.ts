876ffdb7141ef971ae24b98016cf1429fc952e40
Beowulf <beowulf@beocode.eu>
2025-04-19 07:38:47 +0200

    fix(ui): use correct branch name in branch tag selector
    Regression from: 3f673ce4d4665eb2251206bb5ee6c05c2aac0625

--- tests/e2e/repo-home.test.e2e.ts
+++ tests/e2e/repo-home.test.e2e.ts
@@ -27,7 +27,9 @@ test('Branch selector commit icon', async ({page}) => {
   expect(response?.status()).toBe(200);
 
   await expect(page.locator('.branch-dropdown-button svg.octicon-git-branch')).toBeVisible();
+  await expect(page.locator('.branch-dropdown-button')).toHaveText('master');
 
-  await page.goto('/user2/repo1/src/commit/65f1bf27bc');
+  await page.goto('/user2/repo1/src/commit/65f1bf27bc3bf70f64657658635e66094edbcb4d');
   await expect(page.locator('.branch-dropdown-button svg.octicon-git-commit')).toBeVisible();
+  await expect(page.locator('.branch-dropdown-button')).toHaveText('65f1bf27bc');
 });
