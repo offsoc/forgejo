7e489eed25f65a022ffc6503d74930943bee3a89
Earl Warren <contact@earl-warren.org>
2025-04-25 09:14:33 +0000

    chore: merge tests.AddFixtures and unittest.OverrideFixtures (#7648)
    The only parameter that is ever used is a single directory, make it that only instead of a more complex option structure.
    
    Remove tests.AddFixtures that was the simpler form because it is now redundant.
    
    ---
    
    Backporting to v11.0 will help with automated backporting of bug fixes in need of custom made fixtures.
    
    Reviewed-on: https://codeberg.org/forgejo/forgejo/pulls/7648
    Reviewed-by: Michael Kriese <michael.kriese@gmx.de>
    Co-authored-by: Earl Warren <contact@earl-warren.org>
    Co-committed-by: Earl Warren <contact@earl-warren.org>

--- models/packages/main_test.go
+++ models/packages/main_test.go
@@ -4,27 +4,15 @@
 package packages
 
 import (
-	"path/filepath"
 	"testing"
 
 	"forgejo.org/models/unittest"
-	"forgejo.org/modules/setting"
 
 	_ "forgejo.org/models"
 	_ "forgejo.org/models/actions"
 	_ "forgejo.org/models/activities"
 	_ "forgejo.org/models/forgefed"
 )
-
-func AddFixtures(dirs ...string) func() {
-	return unittest.OverrideFixtures(
-		unittest.FixturesOptions{
-			Dir:  filepath.Join(setting.AppWorkPath, "models/fixtures/"),
-			Base: setting.AppWorkPath,
-			Dirs: dirs,
-		},
-	)
-}
 
 func TestMain(m *testing.M) {
 	unittest.MainTest(m)
