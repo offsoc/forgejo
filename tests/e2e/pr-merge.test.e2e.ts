875dd1fe80f5b526e497cc1b1cb8558751687277
0ko <0ko@noreply.codeberg.org>
2025-05-04 23:33:40 +0500

    rebased+linted

--- tests/e2e/pr-merge.test.e2e.ts
+++ tests/e2e/pr-merge.test.e2e.ts
@@ -4,21 +4,21 @@
 
 import {expect} from '@playwright/test';
 import {test, save_visual} from './utils_e2e.ts';
-//import {validate_form} from './shared/forms.ts';
+// import {validate_form} from './shared/forms.ts';
 
 test.use({user: 'user2'});
 
 test.describe('PR: merge', () => {
-  test('with merge commit', async ({page}, {project}) => {
-    //test.skip(project.name === 'Mobile Safari', 'Cannot get it to work - as usual');
+  test('with merge commit', async ({page}) => {
+    // test.skip(project.name === 'Mobile Safari', 'Cannot get it to work - as usual');
     const response = await page.goto('/user2/pr-def-merge-style-merge');
     expect(response?.status()).toBe(200);
 
-    //await validate_form({page}, 'fieldset');
+    // await validate_form({page}, 'fieldset');
 
     // verify header is new
     await expect(page.locator('h4')).toContainText('new'); //
     await page.locator('input[name="rule_name"]').fill('testrule'); //
-    //await save_visual(page);
+    await save_visual(page);
   });
 });
