0c7dc5f13656740de488e36f05c0d2c5cf96483e
Yaourt <codeberg@yaourtprod.net>
2025-05-10 20:58:29 +0200

    e2e test: do not login, reuse user2

--- tests/e2e/user-prefilled-token.test.e2e.ts
+++ tests/e2e/user-prefilled-token.test.e2e.ts
@@ -4,15 +4,12 @@
 // @watch end
 
 import {expect} from '@playwright/test';
-import {test, save_visual, login_user, login} from './utils_e2e.ts';
+import {test, save_visual} from './utils_e2e.ts';
 
-test.beforeAll(async ({browser}, workerInfo) => {
-  await login_user(browser, workerInfo, 'user2');
-});
+// reuse user2 token from scope `shared`
+test.use({user: 'user2', authScope: 'shared'});
 
-test('User: Pre-filled Token name only', async ({browser}, workerInfo) => {
-  const page = await login({browser}, workerInfo);
-
+test('User: Pre-filled Token name only', async ({page}) => {
   const token_name = 'My Token';
   await page.goto(encodeURI(`/user/settings/applications?token_name=${token_name}`));
 
@@ -23,9 +20,7 @@ test('User: Pre-filled Token name only', async ({browser}, workerInfo) => {
   await expect(page.getByText('Review the following preset scope carefully')).toBeHidden();
 });
 
-test('User: Pre-filled Token name and scope', async ({browser}, workerInfo) => {
-  const page = await login({browser}, workerInfo);
-
+test('User: Pre-filled Token name and scope', async ({page}) => {
   const token_name = 'My Token';
   const scope_user = 'read';
   const scope_repository = 'read';
