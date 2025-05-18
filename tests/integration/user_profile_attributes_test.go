0a1e58d2131ab0f9e2aacb589dd8e7819233907a
0ko <0ko@noreply.codeberg.org>
2025-05-19 00:09:14 +0500

    feat: update integr testing

--- tests/integration/user_profile_attributes_test.go
+++ tests/integration/user_profile_attributes_test.go
@@ -16,15 +16,15 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-// TestUserProfileActivity ensures visibility and correctness of elements related to activity of a user:
-// - RSS feed button (doesn't test `other.ENABLE_FEED:false`)
+// TestUserProfileAttributes ensures visibility and correctness of elements related to activity of a user:
+// - RSS/atom feed links (doesn't test `other.ENABLE_FEED:false`) and a few other links nearby
 // - Public activity tab
 // - Banner/hint in the tab
 // - "Configure" link in the hint
 // These elements might depend on the following:
 // - Profile visibility
 // - Public activity visibility
-func TestUserProfileActivity(t *testing.T) {
+func TestUserProfileAttributes(t *testing.T) {
 	defer test.MockVariableValue(&setting.AppSubURL, "/sub")()
 	defer tests.PrepareTestEnv(t)()
 	// This test needs multiple users with different access statuses to check for all possible states
@@ -38,10 +38,10 @@ func TestUserProfileActivity(t *testing.T) {
 	// Set activity visibility of user2 to public. This is the default, but won't hurt to set it before testing.
 	testChangeUserActivityVisibility(t, userRegular, "off")
 
-	// Verify availability of RSS button and activity tab
-	testUser2ActivityButtonsAvailability(t, userAdmin, true)
-	testUser2ActivityButtonsAvailability(t, userRegular, true)
-	testUser2ActivityButtonsAvailability(t, userGuest, true)
+	// Verify availability of activity tab and other links
+	testUser2ActivityLinksAvailability(t, userAdmin, true, true, false)
+	testUser2ActivityLinksAvailability(t, userRegular, true, false, true)
+	testUser2ActivityLinksAvailability(t, userGuest, true, false, false)
 
 	// Verify the hint for all types of users: admin, self, guest
 	testUser2ActivityVisibility(t, userAdmin, "This activity is visible to everyone, but as an administrator you can also see interactions in private spaces.", true)
@@ -63,15 +63,15 @@ func TestUserProfileActivity(t *testing.T) {
 	// Set profile visibility of user2 back to public
 	testChangeUserProfileVisibility(t, userRegular, structs.VisibleTypePublic)
 
-	// = Private acitivty =
+	// = Private activity =
 
 	// Set activity visibility of user2 to private
 	testChangeUserActivityVisibility(t, userRegular, "on")
 
-	// Verify availability of RSS button and activity tab
-	testUser2ActivityButtonsAvailability(t, userAdmin, true)
-	testUser2ActivityButtonsAvailability(t, userRegular, true)
-	testUser2ActivityButtonsAvailability(t, userGuest, false)
+	// Verify availability of activity tab and other links
+	testUser2ActivityLinksAvailability(t, userAdmin, true, true, false)
+	testUser2ActivityLinksAvailability(t, userRegular, true, false, true)
+	testUser2ActivityLinksAvailability(t, userGuest, false, false, false)
 
 	// Verify the hint for all types of users: admin, self, guest
 	testUser2ActivityVisibility(t, userAdmin, "This activity is visible to you because you're an administrator, but the user wants it to remain private.", true)
@@ -112,10 +112,7 @@ func testUser2ActivityVisibility(t *testing.T, session *TestSession, hint string
 	hintLink, hintLinkExists := page.Find("#visibility-hint a").Attr("href")
 
 	// Check that the hint aligns with the actual feed availability
-	assert.Equal(t, availability, page.Find("#activity-feed").Length() > 0)
-
-	// Check availability of RSS feed button too
-	assert.Equal(t, availability, page.Find("#profile-avatar-card a[href='/sub/user2.rss']").Length() > 0)
+	page.AssertElement(t, "#activity-feed", availability)
 
 	// Check that the current tab is displayed and is active regardless of it's actual availability
 	// For example, on /<user> it wouldn't be available to guest, but it should be still present on /<user>?tab=activity
@@ -126,10 +123,21 @@ func testUser2ActivityVisibility(t *testing.T, session *TestSession, hint string
 	return ""
 }
 
-// testUser2ActivityButtonsAvailability checks visibility of Public activity tab on main profile page
-func testUser2ActivityButtonsAvailability(t *testing.T, session *TestSession, buttons bool) {
+// testUser2ActivityLinksAvailability checks visibility of:
+// * Public activity tab on main profile page
+// * user details, profile edit, feed links
+func testUser2ActivityLinksAvailability(t *testing.T, session *TestSession, activity, adminLink, editLink bool) {
 	t.Helper()
 	response := session.MakeRequest(t, NewRequest(t, "GET", "/user2"), http.StatusOK)
 	page := NewHTMLParser(t, response.Body)
-	assert.Equal(t, buttons, page.Find("overflow-menu .item[href='/sub/user2?tab=activity']").Length() > 0)
+	page.AssertElement(t, "overflow-menu .item[href='/sub/user2?tab=activity']", activity)
+
+	// User details - for admins only
+	page.AssertElement(t, "#profile-avatar-card a[href='/sub/admin/users/2']", adminLink)
+	// Edit profile - for self only
+	page.AssertElement(t, "#profile-avatar-card a[href='/sub/user/settings']", editLink)
+
+	// Feed links
+	page.AssertElement(t, "#profile-avatar-card a[href='/sub/user2.rss']", activity)
+	page.AssertElement(t, "#profile-avatar-card a[href='/sub/user2.atom']", activity)
 }
