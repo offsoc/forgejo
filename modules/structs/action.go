d7b6e548288efa88041f5818d9cc008c03d45c05
christopher-besch <mail@chris-besch.com>
2025-05-26 07:20:24 +0200

    feat: ActionRun comments

--- modules/structs/action.go
+++ modules/structs/action.go
@@ -32,38 +32,49 @@ type ActionRunJob struct {
 // swagger:model
 type ActionRun struct {
 	// the action run id
-	ID    int64       `json:"id"`
-	Title string      `json:"title"`
-	Repo  *Repository `json:"repository"`
+	ID int64 `json:"id"`
+	// the action run's title
+	Title string `json:"title"`
+	// the repo this action is part of
+	Repo *Repository `json:"repository"`
 	// the name of workflow file
 	WorkflowID string `json:"workflow_id"`
 	// a unique number for each run of a repository
-	Index       int64 `json:"index_in_repo"`
+	Index int64 `json:"index_in_repo"`
+	// the user that triggered this action run
 	TriggerUser *User `json:"trigger_user"`
-	ScheduleID  int64
-	// the commit/tag/… that caused the run
-	PrettyRef    string `json:"prettyref"`
-	IsRefDeleted bool   `json:"is_ref_deleted"`
-	CommitSHA    string `json:"commit_sha"`
+	// the cron id for the schedule trigger
+	ScheduleID int64
+	// the commit/tag/… the action run ran on
+	PrettyRef string `json:"prettyref"`
+	// has the commit/tag/… the action run ran on been deleted
+	IsRefDeleted bool `json:"is_ref_deleted"`
+	// the commit sha the action run ran on
+	CommitSHA string `json:"commit_sha"`
 	// If this is triggered by a PR from a forked repository or an untrusted user, we need to check if it is approved and limit permissions when running the workflow.
 	IsForkPullRequest bool `json:"is_fork_pull_request"`
 	// may need approval if it's a fork pull request
 	NeedApproval bool `json:"need_approval"`
-	// who approved
-	// TODO: should this be a user?
-	// TODO: or should this just be removed?
+	// who approved this action run
 	ApprovedBy int64 `json:"approved_by"`
 	// the webhook event that causes the workflow to run
-	Event        string `json:"event"`
+	Event string `json:"event"`
+	// the payload of the webhook event that causes the workflow to run
 	EventPayload string `json:"event_payload"`
 	// the trigger event defined in the `on` configuration of the triggered workflow
 	TriggerEvent string `json:"trigger_event"`
-	Status       string `json:"status"`
-	// Started and Stopped is used for recording last run time, if rerun happened, they will be reset to 0
-	Started  time.Time     `json:"started,omitempty"`
-	Stopped  time.Time     `json:"stopped,omitempty"`
-	Created  time.Time     `json:"created,omitempty"`
-	Updated  time.Time     `json:"updated,omitempty"`
+	// the current status of this run
+	Status string `json:"status"`
+	// when the action run was started
+	Started time.Time `json:"started,omitempty"`
+	// when the action run was stopped
+	Stopped time.Time `json:"stopped,omitempty"`
+	// when the action run was created
+	Created time.Time `json:"created,omitempty"`
+	// when the action run was last updated
+	Updated time.Time `json:"updated,omitempty"`
+	// how long the action run ran for
 	Duration time.Duration `json:"duration,omitempty"`
-	HTMLURL  string        `json:"run_html_url"`
+	// the url of this action run
+	HTMLURL string `json:"html_url"`
 }
