c5c2e8c34c3d1583ee54f6dad0722d1cc94f081f
Lucas Schwiderski <lucas@lschwiderski.de>
2025-06-03 16:22:08 +0200

    Add additional tests for pull commit view

--- tests/integration/pull_commit_test.go
+++ tests/integration/pull_commit_test.go
@@ -31,3 +31,20 @@ func TestListPullCommits(t *testing.T) {
 	}
 	assert.Equal(t, "4a357436d925b5c974181ff12a994538ddc5a269", pullCommitList.LastReviewCommitSha)
 }
+
+func TestPullCommitLinks(t *testing.T) {
+	defer tests.PrepareTestEnv(t)()
+
+	req := NewRequest(t, "GET", "/user2/repo1/pulls/3/commits")
+	resp := MakeRequest(t, req, http.StatusOK)
+
+	htmlDoc := NewHTMLParser(t, resp.Body)
+
+	commitSha := htmlDoc.Find(".commit-list td.sha a.sha.label").First()
+	commitShaHref, _ := commitSha.Attr("href")
+	assert.Equal(t, "/user2/repo1/pulls/3/commits/5f22f7d0d95d614d25a5b68592adb345a4b5c7fd", commitShaHref)
+
+	commitLink := htmlDoc.Find(".commit-list td.message a").First()
+	commitLinkHref, _ := commitLink.Attr("href")
+	assert.Equal(t, "/user2/repo1/pulls/3/commits/5f22f7d0d95d614d25a5b68592adb345a4b5c7fd", commitLinkHref)
+}
